cdd9a3942f9aa94179e9495a168311e09e960e27
Real-Bob <ArielSaltz@gmail.com>
2024-08-01 11:22:13 -0400

    Made Rush Spawn Regularly
    Rush now spawns regularly in the backdoors (every 25-60 seconds)
    Rush now deletes itself after passing the map

--- Assets/Scripts/RushMovement.cs
+++ Assets/Scripts/RushMovement.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 public class RushMovement : MonoBehaviour
 {
     private Rigidbody2D RushBody2;
+    Transform _player_transform;
     // Start is called before the first frame update
     void Start()
     {
         RushBody2 = GetComponent<Rigidbody2D>();
         RushBody2.velocity = new Vector2(0f, -15f);
+        _player_transform = GameObject.Find("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        RushBody2.position = new Vector2(_player_transform.position.x,RushBody2.position.y);
+        if (RushBody2.position.y < -1500f)
+        {
+            Destroy(gameObject);
+        }
         
 
         
--- Assets/Scripts/RushSpawner.cs
+++ Assets/Scripts/RushSpawner.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class RushSpawner : MonoBehaviour
 {
+    private Transform RushSpawner2;
+    public GameObject preFab;
+
+    public float cooldown = 30;
     // Start is called before the first frame update
     void Start()
     {
-        
+        RushSpawner2 = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (cooldown > 0)
+        {
+            cooldown -= Time.deltaTime;
+        }
+        else
+        {
+            GameObject rush = Instantiate(preFab,RushSpawner2.position,Quaternion.identity);
+            cooldown = Random.Range(25, 60);
+        }
         
     }
 }

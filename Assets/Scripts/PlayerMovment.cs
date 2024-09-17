d4578c8ede222f7a21afa7440352bb29e68cd923
Real-Bob <ArielSaltz@gmail.com>
2024-09-17 19:48:52 -0400

    Added (Currently just a test) Figure room

--- Assets/Scripts/PlayerMovment.cs
+++ Assets/Scripts/PlayerMovment.cs
@@ -6,18 +6,19 @@ using UnityEngine.SceneManagement;
 
 public class PlayerMovment : MonoBehaviour
 {
-    private Rigidbody2D _rigRigidbody2D;
-    private PlayerDialogue _playerDialogue;
-    private PlayerHide _playerHide;
-    private float _xVelocity = 0f;
-    private float _yVelocity = 0f;
-    public float speed = 3;
-    private const string finishTag = "Finish";
-    private const string escapeTag = "Escape";
-    private const string rushTag = "Rush";
-    public string scaryLevel = "BackDoor";
-    public string realLevel = "FirstLevel";
-    public string rushJumpscare = "RushJumpscare";
+    private Rigidbody2D _rigRigidbody2D; // The actual Rigid Body Object
+    private PlayerDialogue _playerDialogue; //Player Dialouge script
+    private PlayerHide _playerHide; // Player Hide script
+    private float _xVelocity = 0f; //X velocity
+    private float _yVelocity = 0f; //Y velocity
+    public float speed = 3; //Player Speed
+    private const string finishTag = "Finish"; // tag for the collision of the door to the scary room
+    private const string escapeTag = "Escape"; // tag for the collision of the exit of the scary room
+    private const string rushTag = "Rush"; // tag for Rush's collision
+    public string scaryLevel = "BackDoor"; // Tag for the actual scary level scene
+    public string realLevel = "FirstLevel"; // tag for the main world scene
+    public string rushJumpscare = "RushJumpscare"; // tag for the Rush Jumpscare scene
+    public bool isCrouching = false;
     void OnTriggerEnter2D(Collider2D col)
     {
         string colTag = col.tag;
@@ -63,6 +64,11 @@ public class PlayerMovment : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            isCrouching = !isCrouching;
+            speed = (isCrouching? 4:12);
+        }
         if (_playerDialogue.IsSpeaking() | _playerHide.isHidden)
         {
             _xVelocity = 0;
